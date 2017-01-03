cc870dcc6947bc338b55c4643c072b0d5140a0b8
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-01-03 17:14:25 +0100

    Set and retrieve Cell values in the Grid

--- lib/grid.ex
+++ lib/grid.ex
@@ -5,6 +5,9 @@ defmodule Sudoku.Grid do
       iex> grid = Sudoku.Grid.new
       iex> Sudoku.Grid.solved?(grid)
       false
+      iex> grid = Sudoku.Grid.has_known_value(grid,1,3,5)
+      iex> Sudoku.Grid.cell(grid,1,3) |> Sudoku.Cell.value_of
+      5
   """
 
   @rows  3
@@ -18,5 +21,19 @@ defmodule Sudoku.Grid do
   @doc "A Grid is solved when all of its Cells have a known value"
   def solved?(grid) do
     Enum.all?(grid,fn(cell) -> Sudoku.Cell.has_known_value?(cell) end)
+  end
+
+  @doc "Return a grid with a Cell with the given value at the [row,column] coordinates"
+  def has_known_value(grid,row,column,value) do
+    List.replace_at(grid,slot(row,column),Sudoku.Cell.with_known_value(value))
+  end
+
+  @doc "Return the Cell at the [row,column] coordinates"
+  def cell(grid,row,column) do
+    Enum.at(grid,slot(row,column))
+  end
+
+  defp slot(row,column) do
+    row * @columns + column
   end
 end
--- test/grid_test.exs
+++ test/grid_test.exs
@@ -6,4 +6,11 @@ defmodule GridGridTest do
     grid = Sudoku.Grid.new
     assert Sudoku.Grid.solved?(grid) == false
   end
+
+  test "can set a known solution for a cell" do
+    grid = Sudoku.Grid.new
+    |> Sudoku.Grid.has_known_value(1,2,3)
+
+    assert Sudoku.Cell.value_of(Sudoku.Grid.cell(grid,1,2)) == 3
+  end
 end
