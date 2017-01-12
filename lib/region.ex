c471322083f317ea7fce307ac8430f534473aaa1
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-01-12 19:18:50 +0100

    Region is a GenServer

--- lib/region.ex
+++ lib/region.ex
@@ -13,27 +13,47 @@ defmodule Sudoku.Region do
       [{1,2,3}, {2,3,6} , {3,1,5}]
   """
 
-  @doc "Construct a new Region with the given name"
+  use GenServer
+
+# Public API
+
+  @doc "Construct a new Region with the given name and optional list of known values as {row,column,value}"
   def new(name,initial \\ []) do
     grid = Enum.reduce(initial,Sudoku.Grid.new,fn({row,column,value},grid) -> Sudoku.Grid.has_known_value(grid,row,column,value) end)
-    { name, grid }
+   
+    {:ok,pid} = GenServer.start_link( Sudoku.Region, { name, grid })
+    pid
   end
 
   @doc "Returns the name of the region"
   def name(region) do 
-    { name , _grid } = region
-    name
+    GenServer.call(region,:name)
   end
 
   @doc "Returns whether included Grid is solved"
   def solved?(region) do
-    grid(region) |> Sudoku.Grid.solved?
+    GenServer.call(region,:solved?)
   end
 
  @doc "Returns {row,column,value} of all Cells with known value"
  def known_values(region) do
-   grid(region) |> Sudoku.Grid.known_values
+   GenServer.call(region,:known_values)
  end
+
+# GenServer callbacks
+
+  def handle_call(:name,_from,region) do
+    { name , _grid } = region
+    {:reply , name , region }
+  end
+
+  def handle_call(:known_values,_from,region) do
+    {:reply , grid(region) |> Sudoku.Grid.known_values , region}
+  end
+
+  def handle_call(:solved?,_from,region) do
+    {:reply ,  grid(region) |> Sudoku.Grid.solved? , region }
+  end
 
   defp grid(region) do
     { _name , grid } = region
