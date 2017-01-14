dc89818c6b7bff2c1659103d902c99d9aef345f9
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-01-14 13:47:48 +0100

    Include Dialyzer

--- mix.exs
+++ mix.exs
@@ -27,6 +27,8 @@ defmodule ParSudoku.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.14", only: :dev}]
+    [{:ex_doc, "~> 0.14", only: :dev},
+     {:dialyxir, "~> 0.4", only: [:dev], runtime: false}
+    ]
   end
 end
