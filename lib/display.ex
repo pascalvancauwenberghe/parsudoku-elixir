aecdf3a3b88a97fa8bda8ad7d2a3e31198e3e4f0
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-01-19 11:06:33 +0100

    Display receives and stores know values of Regions

--- lib/display.ex
+++ lib/display.ex
@@ -23,10 +23,29 @@ defmodule Sudoku.Display do
     GenServer.call(display,:solved?)
   end
 
+   @doc "Returns list of all received notifications in format {from_region_name , {row, column, value}} in order received"
+   def received(display) do
+     GenServer.call(display,:received)
+   end
+
+   @doc "Notification from region with given role and name that a value {row, column, value} was found"
+   def found(display,role,name,value) do 
+     GenServer.cast(display,{:found, role, name, value})
+   end
+
+  
 # GenServer callbacks
 
   def handle_call(:solved?,_from,state) do
     {:reply ,  length(state) == 81 , state }
+  end
+
+  def handle_call(:received,_from,state) do
+     {:reply , Enum.reverse(state) , state }
+  end
+
+  def handle_cast({:found, _role, name, value},state) do
+    {:noreply , [{name,value}|state] }
   end
 
  end
--- test/display_test.exs
+++ test/display_test.exs
@@ -9,4 +9,13 @@ defmodule DisplayTest do
     assert ! Sudoku.Display.solved?(display)
   end
 
+  test "A display receives known values of Region" do
+    display = Sudoku.Display.new
+
+    region = Sudoku.Region.new("B",[{1,2,3}, {2,3,6} , {3,1,5}]) 
+
+    Sudoku.Region.notify(region,[{:display, display}])
+
+    assert Sudoku.Display.received(display) == [{"B",{1,2,3}}, {"B",{2,3,6}} , {"B",{3,1,5}}]
+  end
 end
