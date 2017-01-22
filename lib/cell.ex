432d166e50690739433aa2014b59877569f4d6f6
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-01-22 14:08:55 +0100

    Specify types of Domain and Cell

--- lib/cell.ex
+++ lib/cell.ex
@@ -36,66 +36,86 @@ defmodule Sudoku.Cell do
       6
   """
 
+  @typedoc """
+  A cell is a { row, column, value}
+  """
+    @type row :: number
+    @type column :: column
+    @type cell :: {row, column, Sudoku.Domain.valuelist}
+
+
+    @spec new(row, column) :: cell
     @doc "Constructor: Create a new cell that can have all possible values in the domain"
     def new(row,column) do
       {row, column, Sudoku.Domain.values}
     end
 
+    @spec with_known_value(row,column,Sudoku.Domain.value) :: cell
     @doc "Constructor: Create a new cell with a given known value"
     def with_known_value(row,column,value) do
       {row, column, [ value ]}
     end
 
+    @spec number_of_possible_values(cell) :: number
     @doc "How many values are possible in this cell?"
     def number_of_possible_values(cell) do
       values(cell) |> length
     end
 
+    @spec has_known_value?(cell) :: boolean
     @doc "A cell has a known value if only one possibility is left"
     def has_known_value?(cell) do
       number_of_possible_values(cell) == 1
     end
 
+    @spec value_of(cell) :: Sudoku.Domain.value
     @doc "Return the value of the cell. Only valid if the value of the cell is known."
     def value_of(cell) do
       values(cell) |> List.first
     end
 
+    @spec can_have_value?(cell,Sudoku.Domain.value) :: boolean 
     @doc "Returns whether the given value is still possible in the cell"
     def can_have_value?(cell,value) do
       values(cell) |> Enum.any?(&(&1 == value))
     end
 
+    @spec cant_have_value(cell,Sudoku.Domain.value) :: cell 
     @doc "Remove the given value from the possible values in the cell"
     def cant_have_value(cell,value) do
       {row,column,values} = cell
       {row,column,List.delete(values,value)}
     end
 
+    @spec cant_have_values(cell,Sudoku.Domain.valuelist) :: cell
     @doc "Remove the given values from the possible values in the cell"
     def cant_have_values(cell,forbidden_values) do
       {row,column,values} = cell
       {row,column,Enum.reject(values,&(Enum.member?(forbidden_values,&1)))}
     end
 
+    @spec set_value(cell,Sudoku.Domain.value) :: cell
     @doc "Set the value of the cell, keeping coordinates"
     def set_value(cell,value) do
       {row,column,_values} = cell
       {row,column,[value]}
     end
 
+    @spec column(cell) :: column
     @doc "Return the column (X) coordinate of the Cell"
     def column(cell) do
       {_row,column,_values} = cell
       column
     end
 
+    @spec row(cell) :: row
     @doc "Return the row (Y) coordinate of the Cell"
     def row(cell) do
       {row,_column,_values} = cell
       row
     end
 
+    @spec values(cell) :: Sudoku.Domain.valuelist
     defp values(cell) do
       {_row,_column,values} = cell
       values
--- lib/domain.ex
+++ lib/domain.ex
@@ -3,13 +3,17 @@ defmodule Sudoku.Domain do
       The domain represents the universe from which possible values of a constraint satisfaction variable are taken
     """
 
+    @type value :: number
+    @type valuelist :: [value]
     @domain [1,2,3,4,5,6,7,8,9]
 
+    @spec size :: number
     @doc "The number of different values in the domain"
     def size do
         length(@domain)
     end
 
+    @spec values :: valuelist
     @doc "The possible values of the domain"
     def values do
       @domain
