3cd6c5e5e404b45dc2a16cd793ee12c4e9bc77da
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-01-23 12:35:11 +0100

    ParSudoku creates 9 regions that can have default values

--- lib/parsudoku.ex
+++ lib/parsudoku.ex
@@ -1,2 +1,20 @@
 defmodule ParSudoku do
+   @moduledoc """
+    The ParSudoku creates the full Sudoku puzzle from 9 `Sudoku.Region`.
+
+      iex> regions = ParSudoku.new([ { "A" , {1,2,3}} ])
+      iex> length(regions)
+      9
+
+   """
+
+  @doc "Create a new puzzle with known initial values [{ region_name, Grid.result} ]"
+  def new(initial) do
+    regions = for char <- ?A .. ?I do
+      name = << char >> 
+      initial_values = initial |> Enum.filter_map(fn({region_name,_result}) -> region_name == name end,fn({_region_name,result}) -> result end)
+      Sudoku.Region.new(name,initial_values)
+    end
+    regions
+  end
 end
--- test/parsudoku_test.exs
+++ test/parsudoku_test.exs
@@ -2,5 +2,13 @@ defmodule ParSudokuTest do
   use ExUnit.Case
   doctest ParSudoku
 
+  test "Parsudoku creates 3x3 Region structure" do
+    regions = ParSudoku.new([ { "A" , {1,2,3}} ])
+
+    assert length(regions) == 9
+    region_a = Enum.at(regions,0)
+
+    assert Sudoku.Region.known_values(region_a) == [ {1,2,3} ]
+  end
 
 end
