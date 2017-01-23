4655fc864d369a5d130e1ba5452dc5403f6647e5
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-01-23 15:07:12 +0100

    Add helper to more easility create Region test data

--- test/parsudoku_test.exs
+++ test/parsudoku_test.exs
@@ -11,4 +11,22 @@ defmodule ParSudokuTest do
     assert Sudoku.Region.known_values(region_a) == [ {1,2,3} ]
   end
 
+  test "Helper function can more easily create region init" do
+    initial = region_initial("C","1_9_2___3")
+
+    assert initial == [ { "C" , {1,1,1}}, { "C" , {1,3,9}}, { "C" , {2,2,2}}, { "C" , {3,3,3}} ]
+  end
+
+  defp region_initial(name,values) do
+    for row <- 1..3 , column <- 1..3 do
+      value = String.at(values,slot(row,column))
+      if value != "_", do: { name , {row, column, String.to_integer(value) } }
+    end
+    |> Enum.filter(&(&1 != nil))
+  end
+
+  defp slot(row,column) do
+    (row - 1) * 3 + (column - 1)
+  end
+
 end
