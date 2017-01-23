8f1dc3537e3b95affdc6a590533945edfede09ec
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2017-01-23 16:22:34 +0100

    Add test helper function to create initial values for set of regions

--- test/parsudoku_test.exs
+++ test/parsudoku_test.exs
@@ -17,6 +17,25 @@ defmodule ParSudokuTest do
     assert initial == [ { "C" , {1,1,1}}, { "C" , {1,3,9}}, { "C" , {2,2,2}}, { "C" , {3,3,3}} ]
   end
 
+  test "Helper function to create a set of regions" do
+    initial = regions_initial(["1_2___5_9", "5462___71", "_3___6_4_" ])
+    assert initial == [ { "A" ,{1, 1, 1}}, {"A", {1, 3, 2}}, {"A", {3, 1, 5}}, {"A", {3, 3, 9}} , 
+                        { "B", {1, 1, 5}}, {"B", {1, 2, 4}}, {"B", {1, 3, 6}}, {"B", {2, 1, 2}}, {"B", {3, 2, 7}}, {"B", {3, 3, 1}}, 
+                        { "C", {1, 2, 3}}, {"C", {2, 3, 6}}, {"C", {3, 2, 4}}]
+  end
+
+  defp regions_initial(values) do
+    parse_regions(values,?A)
+  end
+
+  defp parse_regions([hd|tl],char) do
+    region_initial(<< char >>,hd) ++ parse_regions(tl,char+1)
+  end
+
+  defp parse_regions([],_char) do
+    []
+  end
+
   defp region_initial(name,values) do
     for row <- 1..3 , column <- 1..3 do
       value = String.at(values,slot(row,column))
